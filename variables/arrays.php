3189511d09c87dc712afcf308ad07952efca4afb
Ana <kestaba.dev@gmail.com>
2018-01-30 08:01:09 -0400

    Arreglos.

---
+++ variables/arrays.php
@@ -0,0 +1,13 @@
+<?php
+
+//$arrayVar = ['red', 'blue', 'black'];
+$arrayVar = [
+    'color1' => 'red', 
+    'color2' => 'blue', 
+    3 => 'black'
+];
+//var_dump($arrayVar);
+//var_dump($arrayVar[3]);
+var_dump($arrayVar['color1']);
+
+?>
