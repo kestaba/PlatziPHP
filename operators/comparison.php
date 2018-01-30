11c459e12be371c5efc91e461abe688a19942484
Ana <kestaba.dev@gmail.com>
2018-01-30 14:45:05 -0400

    Comparación.

---
+++ operators/comparison.php
@@ -0,0 +1,14 @@
+<?php
+
+$v1 = 12;
+$v2 = 10;
+
+//$result = $v1 == $v2;
+//$result = $v1 === $v2;
+//$result = $v1 != $v2;
+//$result = $v1 !== $v2;
+$result = $v1 <=> $v2;
+
+var_dump($result);
+
+?>
