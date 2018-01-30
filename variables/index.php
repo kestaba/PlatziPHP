f3680c308b8fc2007124f049ca29d95235cbd96a
Ana <kestaba.dev@gmail.com>
2018-01-30 07:41:53 -0400

    Manejo de variables y tipos de datos.

---
+++ variables/index.php
@@ -0,0 +1,28 @@
+<?php
+
+// int
+//$intVar = 1;
+//var_dump($intVar); //Evaluar el valor de la variable (tipo y valor)
+
+// float
+//$floatVar = 1.2;
+//var_dump($floatVar);
+
+// String
+//$stringVar = 'Sample text';
+//var_dump($stringVar);
+
+
+// bool
+//$boolVar = true;
+//var_dump($boolVar);
+
+// null
+//$nullVar = null;
+//var_dump($nullVar);
+
+// array
+$arrayVar = ['valor1', 12, 1.3];
+var_dump($arrayVar);
+
+?>
