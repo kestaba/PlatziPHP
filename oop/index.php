578be702caf0c1360d01e975783d7e9a4caacb29
Ana <kestaba.dev@gmail.com>
2018-02-01 07:58:00 -0400

    Constructores y destructores.

--- oop/index.php
+++ oop/index.php
@@ -2,7 +2,16 @@
 
 class Car{
     //public $owner;
-    private $owner = 'Mike';
+    private $owner;
+
+    public function __construct($owner){
+        $this->owner = $owner;
+        echo 'construct <br>';
+    }
+
+    public function __destruct(){
+        echo 'destruct <br>';
+    }
 
     public function move(){
         echo 'moving <br>';
@@ -19,16 +28,16 @@ class Car{
 
 echo 'class car <br>';
 
-$car = new Car();
-$car2 = new Car();
+$car = new Car('Alex');
+$car2 = new Car('Mike');
 
 $car->move();
 
 /*$car->owner = 'Alex';
 echo 'Owner: ' . $car->owner;*/
 
-$car->setOwner('Alex');
-$car2->setOwner('Max');
+/*$car->setOwner('Alex');
+$car2->setOwner('Max');*/
 
 echo '<br> Owner: ' . $car->getOwner();
 echo '<br> Owner2: ' . $car2->getOwner();
