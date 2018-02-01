d1ff81f850c76843a67ccca0d59b41e42357f24f
Ana <kestaba.dev@gmail.com>
2018-02-01 05:08:27 -0400

    Cargas de archivos externos,

---
+++ external/index.php
@@ -0,0 +1,10 @@
+<?php
+
+//include 'functions.php';
+//include_once 'functions.php';
+//require 'functions.php';
+require_once 'functions.php';
+
+echo '<p>Text from index.php </p>';
+
+sum(10, 20);
