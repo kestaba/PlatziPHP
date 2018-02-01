808fa2186e3710f64cddbba90e8d294c0688104d
Ana <kestaba.dev@gmail.com>
2018-02-01 04:38:56 -0400

    Estructuras de ciclos.

---
+++ loops/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/*for ($i = 0; $i <= 10; $i++){
+    echo $i . '<br>';
+}*/
+
+//$i = 0;
+
+/*while ($i <=10 ){
+    echo $i++ . '<br>';    
+}*/
+
+/*do{
+    echo $i++ . '<br>';    
+}while ($i <= 10);*/
+
+$names = ['Alex', 'Elizabeth', 'Mary'];
+
+//foreach ($names as $name){
+foreach ($names as $key => $name){
+    echo $key.' - '.$name. '<br>';
+}
+
+
+
