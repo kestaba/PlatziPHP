b22349d790847648f984f8a7a404dfa5092cc088
Ana <kestaba.dev@gmail.com>
2018-02-07 23:29:47 -0400

    Logout de usuarios.

--- blog/app/controllers/AuthController.php
+++ blog/app/controllers/AuthController.php
@@ -40,4 +40,9 @@ class AuthController extends BaseController{
             'errors' => $errors
         ]);
     }
+
+    public function getLogout(){
+        unset($_SESSION['userId']);
+        header('Location: ' . BASE_URL . 'auth/login');
+    }
 }
